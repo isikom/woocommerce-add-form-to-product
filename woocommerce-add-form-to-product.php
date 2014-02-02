81f6bba8d2b93f2bf18969bea65c7f7108fb6a48
Michele Menciassi <m.menciassi@gmail.com>
2014-02-02 14:52:33 +0100

    add name and description

---
+++ woocommerce-add-form-to-product.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Add Form to Product
+ * Plugin URI: http://www.isikom.net/
+ * Description: Add a custom text form to an item. This is required when item in your shop need to get a custom text, for example wedding invitations, plates, serigraphs for pens and more
+ * Author: Michele Menciassi
+ * Author URI: https://plus.google.com/+MicheleMenciassi
+ * Version: 0.0.2
+ * License: GPLv2 or later
+ */
+?>
