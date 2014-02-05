c1b65d0ed069d77abbaa93bb9c4478d0d8747258
Michele Menciassi <m.menciassi@gmail.com>
2014-02-05 12:27:06 +0100

    add js to manage add/remove form to item

---
+++ js/script-admin.js
@@ -0,0 +1,34 @@
+jQuery(document).ready(function($){
+	$('#af2p_add_form_button').on('click', function(){
+		var name = $('#af2p_add_form').val();
+		if (name.length > 0){
+			var title = $('#af2p_add_form  option:selected').text();
+			console.log(name);
+			console.log(title);
+			
+			$( "#af2p_add_form option:selected" ).remove();
+			
+			$('#forms_selected .solo').hide();
+			$('<tr><td><a class="remove dashicons" data-item="'+name+'"></a></td><td>'+title+'</td></tr>').appendTo($('#forms_selected tbody'));			
+		}
+		return false;
+	});
+	$('#forms_selected').on('click', 'a.remove', function(){
+		//alert('remove fired');
+		var numitems = $('#forms_selected a.remove').length;
+		console.log('number of element ' + numitems);
+
+		var name = $(this).attr('data-item');
+        var title = $(this).closest('tr').find('td:nth-child(2)').html();
+
+		var o = new Option(title, name);
+		$("#af2p_add_form").append(o);
+
+		$(this).closest('tr').remove();
+		if (numitems == 1){
+			$('#forms_selected .solo').show();
+		}
+		return false;
+	});
+
+});
--- woocommerce-add-form-to-product.php
+++ woocommerce-add-form-to-product.php
@@ -8,4 +8,179 @@
  * Version: 0.0.2
  * License: GPLv2 or later
  */
+ 
+ // Exit if accessed directly
+if (!defined('ABSPATH'))
+  exit;
+
+//Checks if the WooCommerce plugins is installed and active.
+if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
+	if (!class_exists('WooCommerce_AddFormToProduct')) {
+		class WooCommerce_AddFormToProduct {
+			var $statuses = array('waiting-text', 'waiting-approval-preview');
+			
+			/**
+			 * Gets things started by adding an action to initialize this plugin once
+			 * WooCommerce is known to be active and initialized
+			 */
+			public function __construct() {
+				add_action('woocommerce_init', array(&$this, 'init'));
+			}
+
+			/**
+			 * to add the necessary actions for the plugin
+			 */
+			public function init() {
+				if (is_admin()){
+			        // backend stuff
+			        add_action('woocommerce_product_write_panel_tabs', array(&$this, 'product_write_panel_tab'));
+			        add_action('woocommerce_product_write_panels', array(&$this, 'product_write_panel'));
+			        //add_action('woocommerce_process_product_meta', array($this, 'product_save_data'), 10, 2);
+			        add_action( 'admin_enqueue_scripts', array(&$this, 'wooaf2p_add_admin_scripts'));
+				}else{
+			        // frontend stuff
+				}
+				add_shortcode("text_submition", array(&$this, 'textSubmitionShortcode'));					
+		    }
+			
+			/**
+			 * run on plugin activation 
+			 */
+			public function activation() {
+				//TODO
+				/*
+				foreach($statuses as $status) {
+					wp_insert_term($status, 'shop_order_status');
+				}
+				*/				
+			}
+
+			/**
+			 * run on plugin uninstall 
+			 */
+			public function uninstall() {
+			}
+
+			/**
+			 * run on plugin deactivation 
+			 */
+			public function deactivation() {
+				//TODO
+				/*
+				foreach($statuses as $status) {
+					wp_delete_term($status,'shop_order_status');
+				}
+				*/
+			}
+
+			/**
+			 * shortcode that manage the text submition page 
+			 */
+			public function textSubmitionShortcode($atts, $content = null) {
+				//extract(shortcode_atts(array(
+				//   'href' => 'http://'
+				//), $atts));
+				//return '<a href="'.$href.'">'.$content.'</a>';
+				$order_id = intval($_REQUEST['order']) > 0 ? intval($_REQUEST['order']) : '';
+				$order = new WC_Order();
+				$html = "<h2>pippo $order_id </h2>";
+				if ($order->get_order($order_id)){
+					$html .= "l'ordine esiste";
+					$html .= "<br>Status: " . $order->status;
+					$html .= "<br>User: " . $order->customer_user;
+					$html .= "<br>Key: " . $order->order_key;
+					print_r($order); 
+				}else{
+					$html .= "l'ordine non esiste";
+				}
+				return $html;
+			}
+
+			/**
+			 * creates the tab for the administrator, where administered product sample.
+			 */
+			public function product_write_panel_tab() {
+				echo "<li><a class='added_af2p' href=\"#af2p_tab\">" . __('Form','woo_af2p') . "</a></li>";
+			}
+
+			/**
+			 * build the panel for the administrator.
+			 */
+			public function product_write_panel() {
+	        	global $post;
+				if (in_array('contact-form-7/wp-contact-form-7.php', apply_filters('active_plugins', get_option('active_plugins')))) {
+					$WPCF7 = new WPCF7_ContactForm();
+					$forms = $WPCF7->find();
+					//print_r($forms);
+					$af2p_enable = get_post_meta($post->ID, 'af2p_enamble', true) ? get_post_meta($post->ID, 'af2p_enamble', true) : false;
+					$af2p_forms = get_post_meta($post->ID, 'af2p_forms', true) ? get_post_meta($post->ID, 'af2p_forms', true) : array();
+					?>
+					<div id="af2p_tab" class="panel woocommerce_options_panel">
+						<p class="form-field af2p_enamble_field ">
+							<label for="af2p_enamble"><?php _e('Enable Form', 'woo_af2p');?></label>
+							<input type="checkbox" class="checkbox" name="af2p_enamble" id="af2p_enamble" value="yes" <?php echo $af2p_enable ? 'checked="checked"' : ''; ?>> <span class="description"><?php _e('Enable or disable form option for this item.', 'woo_af2p'); ?></span>
+						</p>
+						<p class="form-field af2p_add_form_field">
+							<label for="af2p_add_form"><?php _e('Add a form', 'woo_af2p'); ?></label>
+							<select id="af2p_add_form" name="af2p_add_form" class="select">
+								<option value=""><?php _e('select a form', 'woo_af2p'); ?></option>
+								<?php
+								if (!empty($forms) and is_array($forms)){
+									foreach ($forms as $key => $form){
+										echo '<option value="'.$form->name.'">'.$form->title.'</option>';
+									}
+								}
+								?>
+							</select>
+							<br/>
+							<button id="af2p_add_form_button" class="button"><?php _e('add', 'woo_af2p'); ?></button>
+						</p>
+						<table id="forms_selected" class="wp-list-table widefat forms">
+							<caption><?php _e('Selected forms', 'woo_af2p'); ?></caption>
+							<thead>
+							<tr>
+								<th><span class="dashicons dashicons-admin-tools"></span></th>
+								<th><?php _e('form name', 'woo_af2p'); ?></th>
+							</tr>
+							</thead>
+							<tbody>
+							<tr class="solo">
+								<td colspan="2"><?php _e('no form selected', 'woo_af2p'); ?></td>
+							</tr>
+							</tbody>							
+						</table>
+					</div>
+					<?php
+				}else{
+					?>
+					<div id="af2p_tab" class="panel woocommerce_options_panel">
+						<p class="form-field">
+							<?php _e('For add a form to this product you need to download and activate Contact Form 7 plugin', 'woo_af2p');?>
+						</p>
+					</div>
+					<?php					
+				}
+			}
+
+			/**
+			 * Enqueue plugin style-file
+			 */
+			function wooaf2p_add_admin_scripts() {
+				// Respects SSL, style-admin.css is relative to the current file
+				wp_register_style( 'wooaf2p-styles', plugins_url('css/style-admin.css', __FILE__) );
+				wp_register_script( 'wooaf2p-scripts', plugins_url('js/script-admin.js', __FILE__), array('jquery') );
+				wp_enqueue_style( 'wooaf2p-styles' );
+				wp_enqueue_script( 'wooaf2p-scripts' );
+			}
+
+
+		}// end class
+		$woocommerce_af2p = new WooCommerce_AddFormToProduct();
+		
+		register_activation_hook( __FILE__, array( 'WooCommerce_AddFormToProduct', 'activation' ) );
+		register_deactivation_hook( __FILE__, array( 'WooCommerce_AddFormToProduct', 'deactivation' ) );
+		register_uninstall_hook( __FILE__, array( 'WooCommerce_AddFormToProduct', 'uninstall' ) );
+	}
+}
+
 ?>
